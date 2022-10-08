afc470e0467ae01ebd1fe321fa1adcbde6381046
Matthew Yu <myu@cpp.edu>
2022-10-07 20:55:25 -0700

    Comment only

---
+++ tiktaktoe.c
@@ -0,0 +1,20 @@
+//Tic Tac Toe game between 2 players or player vs computer
+
+int main(){
+    // prompt user for game they wish to play
+
+    //read information from console
+
+    //if they chose to play vs a bot, choose the bot layout
+
+    //if they chose to play vs a real human, choose the human layout.
+
+    //each time a player makes a move, display the board and what they picked
+
+    //keep going until a winner or a tie
+
+    //once someone ties or wins, stop the game and declare the winner
+
+    
+    return 0;
+}
