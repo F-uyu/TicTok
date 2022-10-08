c219b9e1abefc59c70fde91901ed38544aceb847
Matthew Yu <myu@cpp.edu>
2022-10-07 21:28:18 -0700

    Initalize methods, global variables, the template

--- tiktaktoe.c
+++ tiktaktoe.c
@@ -1,4 +1,22 @@
 //Tic Tac Toe game between 2 players or player vs computer
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+char board[3][3];
+const char PLAYER1 = 'X';
+const char PLAYER2 = 'O';
+const char COMPUTER = 'O';
+
+void resetBoard();
+void printBoard();
+int checkSpaces();
+void Player1Move();
+void Player2Move();
+void AiMove();
+char checkWinner();
+void printWinner(char);
 
 int main(){
     // prompt user for game they wish to play
@@ -17,4 +35,29 @@ int main(){
 
     
     return 0;
+}
+
+void resetBoard(){
+
+}
+void printBoard(){
+
+}
+int checkSpaces(){
+
+}
+void PlayerMove(){
+
+}
+void Player2Move(){
+
+}
+void AiMove(){
+
+}
+char checkWinner(){
+
+}
+void printWinner(char){
+
 }
